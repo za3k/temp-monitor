dde680f65e990dbb8bf604f7997ecd60838c18ce
Zachary Vance <za3k@za3k.com>
2024-10-11 03:09:36 +0000

    Looking done?

--- display.py
+++ display.py
@@ -1,10 +1,11 @@
 from collections import defaultdict
-import copy
 import datetime
 import pytz
+import statistics
 import struct
 
 EPOCH = datetime.datetime(2024, 1, 1, 0, 0, 0, tzinfo=datetime.UTC)
+TZ    = pytz.timezone('US/Eastern')
 
 # All rooms
 UPSTAIRS = [0, 3, 7, 8]
@@ -23,6 +24,24 @@ class Display():
     def __init__(self, sensors):
         pass
 
+    def update(self, state):
+        for unit in ["c", "f"]:
+            current_temps = self.current_temps(state, unit)
+            high_low = self.high_low(state, unit)
+            hourly = self.hourly(state, unit)
+
+            with open("/var/www/public/pub/status/zigbee_temp.{}.txt".format(unit), "w") as f:
+                f.write("\n-------------\n\n".join([
+                    current_temps,
+                    hourly,
+                    high_low,
+                ]))
+
+    def log(self, event):
+        sensor, ts, record = event
+        ts = self.readable_time(ts)
+        print("{}, Sensor #{}\n  {}\n  {}".format(ts, sensor+1, self.record2human(record), record))
+
     def record2human(self, record):
         struct.unpack
         version, row_id, humid, temp, volt, linkquality, batt = struct.unpack("!BBhhHBB", record)
@@ -30,14 +49,24 @@ class Display():
         temp /= 100
         return "v{} #{} -- {}% humid -- {}°C -- {}V -- {} link -- {}% batt".format(version, row_id, humid, temp, volt, linkquality, batt)
 
-    def log(self, event):
-        sensor, ts, record = event
-        ts = self.readable_time(ts)
-        print("{}, Sensor #{}\n  {}\n  {}".format(ts, sensor+1, self.record2human(record), record))
+    def close(self):
+        pass
 
     @staticmethod
     def readable_time(ts):
-        return ts.astimezone(pytz.timezone('US/Eastern')).strftime("%Y-%m-%d %-I:%M%P")
+        return ts.astimezone(TZ).strftime("%Y-%m-%d %-I:%M%P")
+
+    @staticmethod
+    def readable_date(d):
+        if d == " ":
+            return "          "
+        return d.strftime("%Y-%m-%d")
+
+    @staticmethod
+    def readable_hour(ts):
+        if ts == " ":
+            return "               "
+        return ts.astimezone(TZ).strftime("%Y-%m-%d %I%P").replace(" 0", "  ")
 
     @staticmethod
     def readable_timedelta(td):
@@ -45,7 +74,7 @@ class Display():
         hours = td.seconds // 3600
         minutes = (td.seconds - hours*3600) // 60
         if days > 100:
-            return "offline"
+            return "never"
         elif days > 0:
             return "{} days ago".format(days)
         elif hours > 0:
@@ -53,54 +82,106 @@ class Display():
         else:
             return "{} minutes ago".format(minutes)
 
-    def update(self, state):
-        current_temps = self.current_temps(state)
-        high_low = self.high_low(state)
+    @staticmethod
+    def c2f(x):
+        return x*9/5+32
 
-        with open("/var/www/public/pub/status/zigbee_temp.txt", "w") as f:
-            f.write(current_temps)
-            f.write("\n-------------\n\n")
-            f.write(high_low)
+    @staticmethod
+    def readable_temp(x, unit):
+        if x is None:
+            return "--------"
+        elif x == " ":
+            return "        "
+        elif unit == "f":
+            return "{: >6.2f}°F".format(Display.c2f(x))
+        else:
+            assert unit == "c"
+            return "{: >6.2f}°C".format(x)
 
-    def current_temps(self, state):
+    @staticmethod
+    def readable_humidity(x):
+        if x is None:
+            return "-------"
+        else:
+            return "{: >6.2f}%".format(x)
+
+    @staticmethod
+    def readable_temp_range(lo, hi, unit):
+        if lo is None or hi is None:
+            return "-----------------"
+        elif lo == " ":
+            return "                 "
+        elif unit == "f":
+            return "{: >6.2f} - {: >6.2f}°F".format(Display.c2f(lo), Display.c2f(hi))
+        else:
+            assert unit == "c"
+            return "{: >6.2f} - {: >6.2f}°C".format(lo, hi)
+
+    def current_temps(self, state, unit):
         now = datetime.datetime.now(datetime.UTC)
 
-        current_temps = "Current Temperatures\n"
+        current_temps = "Current Temperature\n"
         current_temps += "  last updated: {}\n".format(self.readable_time(now))
         current_temps += "\n"
-        current_temps += "Sensor                        Temperature  Humidity   Last update\n"
+        current_temps += "Sensor                        Temperature  Humidity    Last update\n"
         for sensor in DISPLAY_ORDER:
-            temperature = state.temps[sensor]
-            humidity = state.humid[sensor]
+            temperature = self.readable_temp(state.temps[sensor] or None, unit)
+            humidity = self.readable_humidity(state.humid[sensor] or None)
             last_update = state.last_update[sensor]
             elapsed = self.readable_timedelta(now - last_update)
             human_readable = state.db_metadata[sensor]["human_readable"][5:]
-            #current_temps += "Sensor #{: >2} -- {: >5.2f}°C -- {: >6.2f}% humid -- {: <10s} -- {}\n".format(sensor+1, temperature, humidity, elapsed, human_readable)
-            current_temps += "{: <27s}   {: >5.2f}°C     {: >6.2f}%     {: <10s}\n".format(human_readable, temperature, humidity, elapsed)
+            current_temps += "{: <27s}   {}     {}     {: <10s}\n".format(human_readable, temperature, humidity, elapsed)
         return current_temps
 
-    def day_graph(self, state):
+    def hourly(self, state, unit):
         now = datetime.datetime.now(datetime.UTC)
-        today = now.astimezone(pytz.timezone('US/Eastern')).date()
+        today = now.astimezone(TZ).date()
         yesterday = today - datetime.timedelta(days=1)
         
         hourly = "Hourly Temperature\n"
         hourly += "  last updated: {}\n".format(self.readable_time(now))
+        hourly += "\n"
 
         def hour_bucket(ts):
-            ts = copy(ts)
-            ts.seconds = 0
-            ts.microseconds = 0
-            ts.minutes = 0
-            return ts
+            return ts.replace(second=0, microsecond=0, minute=0)
+        def format_temp(xs):
+            if xs is None or len(xs) == 0:
+                return self.readable_temp(None, unit)
+            else:
+                return self.readable_temp(statistics.mean(xs), unit)
 
-    def high_low(self, state):
+        report_hours = {groupname: {} for groupname in GROUPS.keys()}
+        buckets = set()
+        for groupname, sensors in GROUPS.items():
+            for sensor in sensors:
+                for ts, temp in state.timeseries[sensor]:
+                    hour = hour_bucket(ts)
+                    buckets.add(hour)
+                    if hour not in report_hours[groupname]:
+                        report_hours[groupname][hour] = []
+
+                    report_hours[groupname][hour].append(temp)
+
+        hourly += "{}   ".format(self.readable_hour(" "))
+        for groupname in GROUPS.keys():
+            hourly += "{: <11s}".format(groupname)
+        hourly += "\n"
+        for hour in sorted(buckets):
+            hourly += "{}   ".format(self.readable_hour(hour))
+            for groupname in GROUPS.keys():
+                hourly += "{}   ".format(format_temp(
+                    report_hours[groupname].get(hour)))
+            hourly += "\n"
+
+        return hourly
+
+    def high_low(self, state, unit):
         now = datetime.datetime.now(datetime.UTC)
 
         high_low = "Historical highs and lows\n"
         high_low += "  last updated: {}\n".format(self.readable_time(now))
 
-        start_date = EPOCH.astimezone(pytz.timezone('US/Eastern')).date()
+        start_date = EPOCH.astimezone(TZ).date()
         day_count = 365*64
 
         min_date, max_date = None, None
@@ -123,24 +204,16 @@ class Display():
                         if max_date is None or date > max_date:
                             max_date = date
         
-        def format_temp_range(lo, hi):
-            if lo is None or hi is None:
-                return "--           "
-            else:
-                return "{: >5.2f}-{: >5.2f}°C".format(lo, hi)
         actual_day_count = (max_date-min_date).days+1
         for line, date in enumerate(min_date + datetime.timedelta(n) for n in reversed(range(actual_day_count))):
             if line % 50 == 0:
-                high_low += "\n2000-00-00   "
+                high_low += "\n{}   ".format(self.readable_date(" "))
                 for g in GROUPS.keys():
-                    high_low += "{: <16s}".format(g)
+                    high_low += "{: <20s}".format(g)
                 high_low += "\n"
-            high_low += "{}   ".format(date.strftime("%Y-%m-%d"))
+            high_low += "{}   ".format(self.readable_date(date))
             for groupname in GROUPS.keys():
-                high_low += format_temp_range(report_lows[groupname].get(date), report_highs[groupname].get(date)) + "   "
+                high_low += self.readable_temp_range(report_lows[groupname].get(date), report_highs[groupname].get(date), unit) + "   "
             high_low += "\n"
             
         return high_low
-
-    def close(self):
-        pass
